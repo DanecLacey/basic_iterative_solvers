d9c4b1bf0cd659b5f1c6d916c750487bf90d9e28
Dane Lacey <daneclacey@gmail.com>
2025-04-09 16:36:36 +0200

    overload 'new' with posix_memaligned version

--- common.hpp
+++ common.hpp
@@ -7,6 +7,73 @@
 #include <iomanip>
 #include <cmath>
 
+#ifndef ALIGNMENT
+#define ALIGNMENT 64
+#endif
+
+void* aligned_malloc (size_t bytesize)
+{
+    int errorCode;
+    void* ptr;
+
+    errorCode =  posix_memalign(&ptr, ALIGNMENT, bytesize);
+
+    if (errorCode) {
+        if (errorCode == EINVAL) {
+            fprintf(stderr,
+                    "Error: Alignment parameter is not a power of two\n");
+            exit(EXIT_FAILURE);
+        }
+        if (errorCode == ENOMEM) {
+            fprintf(stderr,
+                    "Error: Insufficient memory to fulfill the request\n");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    if (ptr == NULL) {
+        fprintf(stderr, "Error: posix_memalign failed!\n");
+        exit(EXIT_FAILURE);
+    }
+
+    return ptr;
+}
+
+// Overload new and delete for alignement
+void * operator new(size_t bytesize)
+{
+	// printf("Overloading new operator with size: %lu\n", bytesize);
+	int errorCode;
+	void* ptr;
+	errorCode =  posix_memalign(&ptr, ALIGNMENT, bytesize);
+
+	if (errorCode) {
+		if (errorCode == EINVAL) {
+			fprintf(stderr,
+					"Error: Alignment parameter is not a power of two\n");
+			exit(EXIT_FAILURE);
+		}
+		if (errorCode == ENOMEM) {
+			fprintf(stderr,
+					"Error: Insufficient memory to fulfill the request for space\n");
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	if (ptr == NULL) {
+		fprintf(stderr, "Error: posix_memalign failed!\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return ptr;
+}
+
+void operator delete(void * p)
+{
+	// printf("Overloading delete operator\n");
+	free(p);
+}
+
 class Stopwatch
 {
 
